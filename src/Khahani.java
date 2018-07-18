e0c95bd6a845efc2ee4f4d142763aeaed2e988df
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 04:20:24 -0700

    Practice 1

---
+++ src/Khahani.java
@@ -0,0 +1,28 @@
+/**
+ * Developer and Teacher: Khahani.com
+ * Course: Android 1 - Student Practice
+ */
+
+public class Khahani {
+    // TODO: 1- use psvm to write main method
+
+    // TODO: 2- please store each value in variables with correct data type:
+    /**
+     *      1- year
+     *      2- month
+     *      3- day
+     *      4- android course price
+     *      5- your code melli
+     *      6- your phone number
+     *      7- your name
+     *      8- your last name
+     *      9- the speed of light (m/s)
+     *      10- The number π (pi)
+     */
+
+    // TODO: 3- please write ten other interesting values and write comment about them
+
+    // eath area in km
+    int eathArea = 6371;
+
+}
