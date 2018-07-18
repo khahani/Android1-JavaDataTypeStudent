3aba75d026704c00da0365fd4fa603b480cb7e12
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 04:38:26 -0700

    todo 1 - use psvm to write main method. note: move rest of codes to the main method scope.

--- src/Khahani.java
+++ src/Khahani.java
@@ -5,24 +5,27 @@
 
 public class Khahani {
     // TODO: 1- use psvm to write main method
+    public static void main(String[] args) {
 
-    // TODO: 2- please store each value in variables with correct data type:
-    /**
-     *      1- year
-     *      2- month
-     *      3- day
-     *      4- android course price
-     *      5- your code melli
-     *      6- your phone number
-     *      7- your name
-     *      8- your last name
-     *      9- the speed of light (m/s)
-     *      10- The number π (pi)
-     */
+        // TODO: 2- please store each value in variables with correct data type:
+        /**
+         *      1- year
+         *      2- month
+         *      3- day
+         *      4- android course price
+         *      5- your code melli
+         *      6- your phone number
+         *      7- your name
+         *      8- your last name
+         *      9- the speed of light (m/s)
+         *      10- The number π (pi)
+         */
 
-    // TODO: 3- please write ten other interesting values and write comment about them
+        // TODO: 3- please write ten other interesting values and write comment about them
 
-    // eath area in km
-    int eathArea = 6371;
+        // 1- eath area in km
+        int eathArea = 6371;
+
+    }
 
 }
